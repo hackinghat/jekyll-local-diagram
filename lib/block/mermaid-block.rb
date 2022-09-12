9741f3e3170ce7e38152085e9e70f3abd5319b22
Steve Knight <steve@hackinghat.com>
2022-09-12 18:24:25 +0100

    Create an abstract base for local diagram generation

--- lib/block/mermaid-block.rb
+++ lib/block/mermaid-block.rb
@@ -1,40 +1,13 @@
 module JekyllLocalDiagram
-  class MermaidBlock < Liquid::Block
+  class MermaidBlock < JekyllLocalDiagram::JekyllLocalDiagramBlock
     def initialize(tag_name, markup, tokens)
       super
-      @html = (markup or '').strip
-      @logger = Logger.new(STDOUT)
-      @logger.level = Logger::DEBUG
+      @ext = 'mmd'
+      @blockclass = 'mermaid'
     end
 
-    def render(context)
-      site = context.registers[:site]
-      name = Digest::MD5.hexdigest(super)
-      path = File.join('assets', 'images', 'uml')
-      type = 'svg'
-      mimetype = 'svg+xml'
-      imgfile = "#{name}.#{type}"
-      imgpath = File.join(site.source, path)
-      if !File.exists?(File.join(imgpath, imgfile))
-        uml = File.join(imgpath, "#{name}.uml")
-        img = File.join(imgpath, imgfile)
-        if File.exists?(img)
-          @logger.debug("File #{imgfile} already exists (#{File.size(img)} bytes)")
-        else
-          FileUtils.mkdir_p(File.dirname(uml))
-          File.open(uml, 'w') { |f|
-            f.write(super)
-          }
-          cmd = "mmdc -i #{uml} -o #{imgpath}"
-          @logger.debug("Executing mermaid command: #{cmd}")
-          system(cmd) or raise "mermaid-cli error: #{super}"
-          site.static_files << Jekyll::StaticFile.new(
-            site, site.source, path, imgfile
-          )
-          @logger.debug("File #{imgfile} created (#{File.size(img)} bytes)")
-        end
-      end
-      "<p><object data='#{site.baseurl}/#{path}/#{imgfile}' type='image/#{mimetype}' #{@html} class='mermaid'></object></p>"
+    def build_cmd(input, output)
+      "mmdc.sh #{input} #{output}"
     end
   end
 end
--- lib/jekyll-local-diagram.rb
+++ lib/jekyll-local-diagram.rb
@@ -5,9 +5,48 @@ require 'digest'
 require 'fileutils'
 require 'logger'
 
+module JekyllLocalDiagram
+    class JekyllLocalDiagramBlock < Liquid::Block
+        def initialize(tag_name, markup, tokens)
+          super
+          @html = (markup or '').strip
+          @logger = Logger.new(STDOUT)
+          @logger.level = Logger::DEBUG
+        end
+    
+        def render(context)
+            site = context.registers[:site]
+            name = Digest::MD5.hexdigest(super)
+            path = File.join('assets', 'images', @ext)
+            type = 'svg'
+            mimetype = 'svg+xml'
+            imgfile = "#{name}.#{type}"
+            imgpath = File.join(site.source, path)
+            if !File.exists?(File.join(imgpath, imgfile))
+              txtfile = File.join(imgpath, "#{name}.#{@ext}")
+              img = File.join(imgpath, imgfile)
+              if File.exists?(img)
+                @logger.debug("File #{imgfile} already exists (#{File.size(img)} bytes)")
+              else
+                FileUtils.mkdir_p(imgpath)
+                File.open(txtfile, 'w') { |f|
+                  f.write(super)
+                }
+                cmd = build_cmd(txtfile, img)
+                @logger.debug("Executing #{@blockclass} command: #{cmd}")
+                system(cmd) or raise "#{@blockclass} error: #{super}"
+                site.static_files << Jekyll::StaticFile.new(
+                  site, site.source, path, imgfile
+                )
+                @logger.debug("File #{imgfile} created (#{File.size(img)} bytes)")
+              end
+            end
+            "<p><object data='#{site.baseurl}/#{path}/#{imgfile}' type='image/#{mimetype}' #{@html} class='#{@blockclass}'></object></p>"
+        end
+    end
+end
+
 require 'block/plantuml-block'
 require 'block/mermaid-block'
 require 'block/mathjax-block'
 
-module JekyllLocalDiagram
-end
